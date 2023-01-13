7dc904c6dbc55e1256f98b6eab08aab4ff246597
wrynnme <y.nnyrw@gmail.com>
2023-01-13 14:46:36 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NZ_I18N } from "ng-zorro-antd/i18n";
+import { en_US, th_TH } from "ng-zorro-antd/i18n";
+import { registerLocaleData } from "@angular/common";
+import en from "@angular/common/locales/en";
+import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { IconsProviderModule } from "./icons-provider.module";
+import { NzLayoutModule } from "ng-zorro-antd/layout";
+import { NzMenuModule } from "ng-zorro-antd/menu";
+import th from "@angular/common/locales/th";
+
+registerLocaleData(en);
+registerLocaleData(th);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		FormsModule,
+		HttpClientModule,
+		BrowserAnimationsModule,
+		IconsProviderModule,
+		NzLayoutModule,
+		NzMenuModule,
+	],
+	providers: [{ provide: NZ_I18N, useValue: th_TH }],
+	bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
