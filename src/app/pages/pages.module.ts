aa421c781016fc1647c4520789002c06bf157fb5
wrynnme <y.nnyrw@gmail.com>
2023-01-13 19:59:35 +0700

    Create pages.module.ts

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ShortenlinkComponent } from './shortenlink/shortenlink.component';
+
+@NgModule({
+	declarations: [
+    ShortenlinkComponent
+  ],
+	imports: [CommonModule],
+})
+export class PagesModule {}
